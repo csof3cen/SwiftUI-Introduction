04add28b04433e2f70106dc926794dd2b87e2eb5
Serigne Moustapha Sy Ndoye <88747372+nyshire@users.noreply.github.com>
2022-07-12 22:21:14 +0000

    Adding assets for Contact App

---
+++ Contacts/Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by S. Moustapha Sy Ndoye on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contacts/Contacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Contacts
+//
+//  Created by S. Moustapha Sy Ndoye on 12/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
